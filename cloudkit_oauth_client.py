e7604ab39791b3317d3d6d7ea4017c8f70a6b759
Jim Pick <jim@jimpick.com>
2009-01-25 23:58:08 -0800

    Works, except for PUT/POST.

--- cloudkit_oauth_client.py
+++ cloudkit_oauth_client.py
@@ -11,6 +11,12 @@ import oauth
 import pickle # for storing access token to file
 import cgi # for playing with query strings
 import urlparse
+import urllib
+
+# url escape
+def escape(s):
+    # escape '/' too
+    return urllib.quote(s, safe='~')
 
 # file to store access token
 TOKEN_FILE = './access_token.pkl'
@@ -42,7 +48,7 @@ def fetch_response(oauth_request, connection, debug=True):
     if debug:
         print 'requested URL: %s' % url
         print 'server response: %s' % s
-    return s
+    return s, response.getheaders()
 
 # main routine
 def test_cloudkit():
@@ -67,7 +73,7 @@ def test_cloudkit():
         oauth_request = oauth.OAuthRequest.from_consumer_and_token(consumer,
             http_method='POST', http_url=REQUEST_TOKEN_URL) # create an oauth request
         oauth_request.sign_request(signature_method, consumer, None) # the request knows how to generate a signature
-        resp=fetch_response(oauth_request, connection) # use our fetch_response method to send the request to CloudKit
+        resp,h=fetch_response(oauth_request, connection) # use our fetch_response method to send the request to CloudKit
         print '\nCloudKit response was: %s' % resp
         # if something goes wrong and you get an unexpected response, you'll get an error on this next line
         token=oauth.OAuthToken.from_string(resp) # parse the response into an OAuthToken object
@@ -91,7 +97,7 @@ def test_cloudkit():
         oauth_request = oauth.OAuthRequest.from_consumer_and_token(consumer, 
             http_method='POST', token=token, http_url=ACCESS_TOKEN_URL)
         oauth_request.sign_request(signature_method, consumer, token)
-        resp=fetch_response(oauth_request, connection) # use our fetch_response method to send the request to CloudKit
+        resp,h=fetch_response(oauth_request, connection) # use our fetch_response method to send the request to CloudKit
         print '\nCloudKit response was: %s' % resp
         # now the token we get back is an access token
         token=oauth.OAuthToken.from_string(resp) # parse the response into an OAuthToken object
@@ -108,24 +114,40 @@ def test_cloudkit():
     print '\n* Access a protected resource ...'
     print 'To try a query enter somthing like: %s' % QUERY_API_URL
     s=pause('enter a URL (empty string or <ENTER> to quit):')
-    while s:
+    m=pause('enter method (GET/POST/PUT/DELETE/OPTIONS or empty string or <ENTER> to quit):')
+    while s and m:
         try:
             (path, query)=s.split('?',1)
             params=clean_params( cgi.parse_qs(query) )
         except ValueError:
             path=s
             params={}
-        oauth_request = oauth.OAuthRequest.from_consumer_and_token(consumer, http_method='POST', token=token, http_url=path, parameters=params)
+        oauth_request = oauth.OAuthRequest.from_consumer_and_token(consumer, http_method=m, token=token, http_url=path, parameters=params)
         oauth_request.sign_request(signature_method, consumer, token)
-        headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"} # need this?
-        post_data=oauth_request.to_postdata()
-        print '\nPOSTing to %s' % path
-        print 'sending headers: %s' % headers
-        print 'sending data: %s' % post_data
-        connection.request('POST', path, post_data, headers)
-        s = connection.getresponse().read()
+        if m == 'POST' or m == 'PUT':
+            headers = oauth_request.to_header()
+            headers["Content-type"] = "application/x-www-form-urlencoded"
+            headers["Accept"] = "text/plain"
+            d=pause('enter data:')
+            post_data = escape(str(d))
+            print '\n%sing to %s' % (m, path)
+            print 'sending headers: %s' % headers
+            print 'sending data: %s' % post_data
+            connection.request(m, path, post_data, headers)
+            r = connection.getresponse()
+            print r.status, r.reason
+            s = r.read()
+        else:
+            full_url=oauth_request.to_url()
+            print '\n%s : %s' % (m, full_url)
+            s,h=fetch_response(oauth_request, connection)
         print '\nthe server says: %s' % s
+        print 'headers:'
+        for hdr in h:
+            print "  %s = %s" % hdr
+        print 'To try a query enter somthing like: %s' % QUERY_API_URL
         s=pause('\nenter a URL (empty string or <ENTER> to quit):')
+        m=pause('enter method (GET/POST/PUT/DELETE/OPTIONS or empty string or <ENTER> to quit):')
         
 # cgi.parse_qs returns values as a list -- we want single values, we'll just keep the first for now...
 def clean_params(p):
